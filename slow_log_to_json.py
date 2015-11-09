c4f9dedb51ac421296c725b58e5154eb4621c7ea
Tiago Caxias <tcx.taliesin@gmail.com>
2015-11-09 14:31:34 +0000

    Changed regex to match weird SQL line starts such as "# comments" and "# SET"

--- slow_log_to_json.py
+++ slow_log_to_json.py
@@ -9,17 +9,20 @@ if vi[0] ==3:
 else:
     from itertools import imap, ifilter, izip
 
-from re import split, sub, compile
+from re import split, sub, compile, M
 from itertools import chain
 from fileinput import input
 from json import dumps
+from datetime import datetime
+from pprint import pprint
+# datetime.datetime.utcfromtimestamp(1446223940).isoformat()
 
-backticks=compile('`')
-metadata=compile('((?:#[^\n]+\n)+)')
-meta_lines=compile('# ([^\n]+)\n')
+metadata=compile('((?:# \w[\w@]+:[^\n]+\n)+)')
+meta_lines=compile('^# ([^\n]+)\n',M)
 meta_keywords=compile('((?:[A-Z][^: ]+)):')
 sql_lines=compile('([^;]+);')
 newline=compile('\n')
+bogus_chars=compile('[\r\t`]+')
 
 def pair_list(l):
     tl=[]
@@ -33,15 +36,22 @@ def hash_list(l):
         h[x]=y
     return h
 
+def hash_list_by_head(l):
+    h={}
+    for x in l:
+        h[x.partition(' ')[0]]=x
+    return h
+
 header=1
 slow_log_bulk=''
 for line in input():
     if header==1 and line[0]=='#':
         header=0
     if header==0:
-        slow_log_bulk+=backticks.sub('',line)
+        slow_log_bulk+=bogus_chars.sub('',line)
 
 metadata_and_data_grouped = pair_list(filter(lambda x : x!='' and x !='\n', metadata.split(slow_log_bulk)))
+# pprint (metadata_and_data_grouped)
 
 slow_log_dict=[]
 for i in metadata_and_data_grouped:
@@ -56,12 +66,13 @@ for i in metadata_and_data_grouped:
                             lambda x : x!='' and x!='\n' ,
                             meta_lines.split(i[0]))))))))
     slow_log_dict[-1].update({'queries':
-        map(str.strip,
-            ifilter(
-                lambda x : x!='' and x!='\n' and x!=' ',
-                    sql_lines.split(
-                        newline.sub(' ',i[1]))))})
+        hash_list_by_head(
+            map(str.strip,
+                ifilter(
+                    lambda x : x!='' and x!='\n' and x!=' ',
+                        sql_lines.split(
+                            newline.sub(' ',i[1])))))})
 #    slow_log_dict[-1].pop('QC_hit',None)
 
 for i in slow_log_dict:
-    print dumps(i)
+    print dumps(i), "\n"
